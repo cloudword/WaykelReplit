78eaa40b4564ee957b6ddaf800a3767fb49428db
Mayank Pratap Singh <mayank@Mayanks-MacBook-Air.local>
2026-01-25 01:09:56 +0530

    Allow CSRF header in CORS

--- server/index.ts
+++ server/index.ts
@@ -90,7 +90,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, X-CSRF-Token');
     res.setHeader('Access-Control-Max-Age', '86400'); // Cache preflight for 24 hours
   }
   
